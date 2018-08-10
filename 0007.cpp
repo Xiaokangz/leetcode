81fe329d57d27f73c36127455965a23241a05d11
xiaokangz00@gmail.com <k9693088@gmail.com>
2018-08-10 21:47:15 +0800

    0007 done

---
+++ 0007.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	int reverse(int x) {
+		int ans = 0;
+		while (x != 0) {
+			int temp = x % 10;
+			x  /= 10;
+			if (ans > INT_MAX / 10 || (ans == INT_MAX && temp > 7)) {
+				return 0;
+			}
+			if (ans < INT_MIN / 10 || (ans == INT_MIN && temp < -8)) {
+				return 0;
+			}
+			ans = ans * 10 + temp;
+		}
+		return ans;
+	}
+}
