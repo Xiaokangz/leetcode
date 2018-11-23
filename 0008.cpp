ee0b3dcfad69aa46c64ad3ce623b7d175efe98cc
Xiaokang Zhang <xiaokangz00@gmail.com>
2018-11-23 20:02:42 +0800

    0008 done

--- 0008.cpp
+++ 0008.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     int myAtoi(string str) {
         if (str.length() == 0) {
-        	return 0;
+            return 0;
         }
         int i;
         int len = str.length();
         for (i = 0; i < len && str[i] == ' '; i++);
         int sign = 1; 
         if (i != len) {
-        	if (str[i] == '+') {
-        		i++;
-        	} else if (str[i] == '-') {
-        		sign = -1;
-        		i++;
-        	}
+            if (str[i] == '+') {
+                i++;
+            } else if (str[i] == '-') {
+                sign = -1;
+                i++;
+            }
         }
 
         double sum = 0;
         while(i < len && isdigit(str[i])) {
-        	int temp = str[i] - '0';
-        	sum = sum * 10 + sign * temp;
-        	if (sum > INT_MAX) {
-        		return INT_MAX;
-        	} else if (sum < INT_MIN) {
-        		return INT_MIN;
-        	}
-        	i++;
+            int temp = str[i] - '0';
+            sum = sum * 10 + sign * temp;
+            if (sum > INT_MAX) {
+                return INT_MAX;
+            } else if (sum < INT_MIN) {
+                return INT_MIN;
+            }
+            i++;
         }
         return (int)sum;
     }
